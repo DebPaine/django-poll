465f2960407fd13832ff193a33784d7479afce15
unknown <debmalyapaine@gmail.com>
2023-12-27 19:51:54 +0530

    add: templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# We use polls namespace so that we know that the below urls are of this app
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world, from polls app!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:3]
+    response = "\n".join([q.question_text for q in latest_question_list])
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking at question {question_id}")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Poll not found!")
+    # return render(request, "polls/detail.html", {"question": question})
+    # Shortcut
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
